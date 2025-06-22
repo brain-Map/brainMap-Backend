4ceb90f6894dc38c0197133ef1b6d5f458559438
sacheex <sachithdhanushka00@gmail.com>
2025-06-22 23:51:11 +0530

    Initial commit

---
+++ src/main/java/com/app/brainmap/BrainMapApplication.java
@@ -0,0 +1,13 @@
+package com.app.brainmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/brainmap/BrainMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.brainmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
