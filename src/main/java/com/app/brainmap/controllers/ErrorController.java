875f7d6b40352b87432e92be8b38b9e8bafc6be1
sachithdh <sachithdhanushka00@gmail.com>
2025-07-01 18:16:46 +0530

    Add authentication controller

---
+++ src/main/java/com/app/brainmap/controllers/ErrorController.java
@@ -0,0 +1,51 @@
+package com.app.brainmap.controllers;
+
+import com.app.brainmap.domain.dto.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.View;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+
+    private final View error;
+
+    public ErrorController(View error) {
+        this.error = error;
+    }
+
+    /**
+     * Handles all exceptions and returns a standardized error response.
+     *
+     * @param e the exception that was thrown
+     * @return a ResponseEntity containing the error details
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception e) {
+        log.error("An error occurred: {}", e);
+        ApiErrorResponse error = new ApiErrorResponse().builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred")
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(e.getMessage())
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+
+}
