3d1b9f9ca8c540a1769985ea4d55891c005ddcb0
sachithdh <sachithdhanushka00@gmail.com>
2025-07-08 14:37:20 +0530

    Add CreateCommunityPostRequest class for community post creation

---
+++ src/main/java/com/app/brainmap/domain/CreateCommunityPostRequest.java
@@ -0,0 +1,22 @@
+package com.app.brainmap.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateCommunityPostRequest {
+    private String title;
+    private String content;
+
+    @Builder.Default
+    private Set<UUID> tagsIds = new HashSet<>();
+}
