54795279b1f352243ac6e3a3050ecb74999a16cb
Nadun Madusanka <nadunmadusanka564@gmail.com>
2025-07-16 17:47:56 +0530

    projecr-member Project list api implement and bug fix

--- src/main/java/com/app/brainmap/controllers/ProjectController.java
+++ src/main/java/com/app/brainmap/controllers/ProjectController.java
@@ -4,10 +4,9 @@ import com.app.brainmap.domain.dto.ProjectDto;
 import com.app.brainmap.domain.entities.Project;
 import com.app.brainmap.mappers.ProjectMapper;
 import com.app.brainmap.services.ProjectService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/project-member/projects")
@@ -19,6 +18,14 @@ public class ProjectController {
     public ProjectController(ProjectService projectService, ProjectMapper projectMapper) {
         this.projectService = projectService;
         this.projectMapper = projectMapper;
+    }
+
+    @GetMapping
+    public List<ProjectDto> listProject() {
+        return projectService.listProject()
+                .stream()
+                .map(projectMapper::toDto)
+                .toList();
     }
 
     @PostMapping
