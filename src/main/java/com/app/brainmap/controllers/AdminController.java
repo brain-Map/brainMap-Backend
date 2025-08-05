e7d2e4c894db75a96dc85d68569aa8863d3bb0b7
dinuka2001 <2022cs047@stu.ucsc.cmb.ac.lk>
2025-08-06 00:39:54 +0530

    Implement user count retrieval in AdminController and UserService

--- src/main/java/com/app/brainmap/controllers/AdminController.java
+++ src/main/java/com/app/brainmap/controllers/AdminController.java
@@ -1,4 +1,25 @@
 package com.app.brainmap.controllers;
 
+import com.app.brainmap.services.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@Slf4j
 public class AdminController {
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users/count")
+    public ResponseEntity<Long> getUserCount() {
+        return ResponseEntity.ok(userService.userCount());
+    }
+
 }
