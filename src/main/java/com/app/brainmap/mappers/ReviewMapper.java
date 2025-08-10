6a108edc84f0430cbdb85cc12e6c80f9230d5b4e
Eraji Thenuwara <thenuwaraeraji@gmail.com>
2025-08-10 13:43:58 +0530

    Review mapper

--- src/main/java/com/app/brainmap/mappers/ReviewMapper.java
+++ src/main/java/com/app/brainmap/mappers/ReviewMapper.java
@@ -2,19 +2,34 @@ package com.app.brainmap.mappers;
 
 import com.app.brainmap.domain.dto.ReviewDto;
 import com.app.brainmap.domain.entities.Review;
+import com.app.brainmap.domain.entities.User;
+import com.app.brainmap.domain.entities.Promise;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.UUID;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ReviewMapper {
-    @Mapping(source = "member.userId", target = "memberId")
-    @Mapping(source = "mentor.userId", target = "mentorId")
-    @Mapping(source = "promise.promiseId", target = "promiseId")
+    @Mapping(source = "member", target = "memberId", qualifiedByName = "mapUserToUserId")
+    @Mapping(source = "mentor", target = "mentorId", qualifiedByName = "mapUserToUserId")
+    @Mapping(source = "promise", target = "promiseId", qualifiedByName = "mapPromiseToPromiseId")
     ReviewDto toDto(Review review);
 
-    @Mapping(target = "member", ignore = true) // To be set manually in the service layer
-    @Mapping(target = "mentor", ignore = true) // To be set manually in the service layer
-    @Mapping(target = "promise", ignore = true) // To be set manually in the service layer
+    @Mapping(target = "member", ignore = true)
+    @Mapping(target = "mentor", ignore = true)
+    @Mapping(target = "promise", ignore = true)
     Review toEntity(ReviewDto dto);
+
+    @Named("mapUserToUserId")
+    default UUID mapUserToUserId(User user) {
+        return user != null ? (UUID) user.getUserId() : null;
+    }
+
+    @Named("mapPromiseToPromiseId")
+    default UUID mapPromiseToPromiseId(Promise promise) {
+        return promise != null ? promise.getPromiseId() : null;
+    }
 }
