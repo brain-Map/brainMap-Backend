a1e518da2d3245969b230c58b2d224db36bb6d98
sachithdh <sachithdhanushka00@gmail.com>
2025-08-18 02:42:53 +0530

    Add CORS configuration and update security headers in SecurityConfig

--- src/main/java/com/app/brainmap/config/SecurityConfig.java
+++ src/main/java/com/app/brainmap/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -42,9 +43,11 @@ public class SecurityConfig {
                             return corsConfig;
                         })
                 )
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .authorizeHttpRequests(auth -> auth
 //                        .requestMatchers(HttpMethod.GET, "/api/v1/auth/test").permitAll()
                         // TODO: Handle permitAll properly once Authentication is implemented
+                                .requestMatchers("/ws/**").permitAll()
                         .anyRequest().permitAll()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class);
