ab40e2a5c3f5d2f89965aaf4b8a5c631a3b94d18
Nadun Madusanka <nadunmadusanka564@gmail.com>
2025-10-03 19:32:28 +0530

    bug fix

--- src/main/java/com/app/brainmap/domain/entities/Community/CommunityTag.java
+++ src/main/java/com/app/brainmap/domain/entities/Community/CommunityTag.java
@@ -1,30 +1,3 @@
-<<<<<<< HEAD:src/main/java/com/app/brainmap/domain/entities/CommunityTag.java
-package com.app.brainmap.domain.entities;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.*;
-
-@Entity
-@Table(name = "community_tags")
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@Builder
-public class CommunityTag {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID tagId;
-
-    @Column(unique = true, nullable = false)
-    private String name;
-
-    @ManyToMany(mappedBy = "tags")
-    private Set<CommunityPost> posts = new HashSet<>();
-=======
 package com.app.brainmap.domain.entities.Community;
 
 import jakarta.persistence.*;
@@ -50,5 +23,4 @@ public class CommunityTag {
 
     @ManyToMany(mappedBy = "tags")
     private Set<CommunityPost> posts = new HashSet<>();
->>>>>>> 8927c80eb6a121132779d0a641aeb7caad6a320c:src/main/java/com/app/brainmap/domain/entities/Community/CommunityTag.java
 }
