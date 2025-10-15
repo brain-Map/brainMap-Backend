8c04e6a5707d56025caa27ac3d25b188f8240f75
Isuru <isurunaveen27@gmail.com>
2025-10-16 00:56:44 +0530

    Add AppConfig class for application configuration properties for payhere

--- src/main/java/com/app/brainmap/BrainMapApplication.java
+++ src/main/java/com/app/brainmap/BrainMapApplication.java
@@ -1,12 +1,13 @@
 package com.app.brainmap;
 
+import com.app.brainmap.config.AppConfig;
 import com.app.brainmap.config.PayHereConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(PayHereConfig.class)
+@EnableConfigurationProperties({PayHereConfig.class, AppConfig.class})
 public class BrainMapApplication {
     public static void main(String[] args) {
         SpringApplication.run(BrainMapApplication.class, args);
