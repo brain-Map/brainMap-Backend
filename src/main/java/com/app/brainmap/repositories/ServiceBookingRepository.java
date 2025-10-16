d7d97cb70bce5b37271ca3f4d33644085fb3851d
NadunMN <nadunmadusanka564@gmail.com>
2025-10-16 10:50:51 +0530

    booked seesion get

--- src/main/java/com/app/brainmap/repositories/ServiceBookingRepository.java
+++ src/main/java/com/app/brainmap/repositories/ServiceBookingRepository.java
@@ -1,7 +1,11 @@
 package com.app.brainmap.repositories;
 
+import com.app.brainmap.domain.dto.ProjectMember.BookingDetailsDto;
 import com.app.brainmap.domain.entities.DomainExpert.ServiceBooking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.UUID;
 import java.util.List;
 
@@ -10,5 +14,26 @@ public interface ServiceBookingRepository extends JpaRepository<ServiceBooking, 
     List<ServiceBooking> findByUserId(UUID userId);
 
     List<ServiceBooking> findByDomainExpert_Id(UUID domainExpertId);
+
+    @Query("""
+    SELECT new com.app.brainmap.domain.dto.ProjectMember.BookingDetailsDto(
+        sb.id,
+        s.id,
+        s.title,
+        sb.status,
+        s.subject,
+        u.firstName,
+        u.lastName,
+        u.email
+    )
+    FROM ServiceBooking sb
+    JOIN sb.service s
+    JOIN sb.domainExpert de
+    JOIN de.user u
+    WHERE sb.user.id = :userId
+    """)
+    List<BookingDetailsDto> findAllBookingDetailsByUserId(UUID userId);
+
+
 }
 
