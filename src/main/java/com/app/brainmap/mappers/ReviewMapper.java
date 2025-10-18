f17a3ea9036c6634c0bfb5616a773df3e2f89e52
sachithdh <sachithdhanushka00@gmail.com>
2025-10-18 12:41:29 +0530

    refactor: Remove Promise and Property entities, and clean up related repositories and service dependencies

--- src/main/java/com/app/brainmap/mappers/ReviewMapper.java
+++ src/main/java/com/app/brainmap/mappers/ReviewMapper.java
@@ -2,14 +2,9 @@ package com.app.brainmap.mappers;
 
 import com.app.brainmap.domain.dto.ReviewDto;
 import com.app.brainmap.domain.entities.Review;
-import com.app.brainmap.domain.entities.User;
-import com.app.brainmap.domain.entities.Promise;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
-
-import java.util.UUID;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ReviewMapper {
